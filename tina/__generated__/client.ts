7dfd9006d23ec5ef302e492fd52742c6910ad445
Dan McColly <DanMcColly@GMail.com>
2025-07-13 16:23:09 -0600

    Fix TinaCMS module resolution: sync CLI and core package versions to 1.6.7

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/danmc/HOIBF_Repo/history-of-idaho-broadcasting/tina/__generated__/.cache/1752445236942', url: 'https://content.tinajs.io/1.6/content/dummy_client_id/github/main', token: 'dummy_token', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/danmc/HOIBF_Repo/history-of-idaho-broadcasting/tina/__generated__/.cache/1752445371224', url: 'https://content.tinajs.io/1.6/content/dummy_client_id/github/main', token: 'dummy_token', queries,  });
 export default client;
   
