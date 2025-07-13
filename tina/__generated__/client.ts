3bfc08914cfbfd0c38394f779eb7d68401a98e44
Dan McColly <DanMcColly@GMail.com>
2025-07-13 17:08:31 -0600

    Fix deployment issues: Remove TinaCMS dependencies, clean build process, and ensure Netlify compatibility

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/danmc/HOIBF_Repo/history-of-idaho-broadcasting/tina/__generated__/.cache/1752447005863', url: 'https://content.tinajs.io/1.6/content/cfe5d0ac-eeff-4222-a763-f5278eaf061b/github/main', token: '3ef431c0f788b528ff5fb3c612119683895bd080', queries,  });
-export default client;
-  
