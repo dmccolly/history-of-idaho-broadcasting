cf3cc2be030ed04746c45c36783c1d7138284ab5
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-07-15 17:56:10 -0600

    Use Storyblok header and client AOS

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,28 +1,16 @@
-"use client"
-
-import { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-import HeaderClient from '@/components/ui/header-client'
+import Header from '@/components/ui/header'
 import FooterClient from '@/components/ui/footer-client'
+import AOSInit from '@/components/aos-init'
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 600,
-      easing: 'ease-out-sine',
-    })
-  })
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      <HeaderClient />
+      <AOSInit />
+      <Header />
       <main className="grow flex">{children}</main>
       <FooterClient />
     </div>
--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,25 +1,13 @@
-"use client"
-
-import { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-import HeaderClient from '@/components/ui/header-client'
+import Header from '@/components/ui/header'
 import FooterClient from '@/components/ui/footer-client'
+import AOSInit from '@/components/aos-init'
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 600,
-      easing: 'ease-out-sine',
-    })
-  })
-
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      <HeaderClient />
+      <AOSInit />
+      {/* Navigation from Storyblok */}
+      <Header />
       <main className="grow">{children}</main>
       <FooterClient />
     </div>
